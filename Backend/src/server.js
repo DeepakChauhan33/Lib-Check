ea700fee88f17af5453f6a503698a71014dce90e
deepakchauhan33 <deep651965@gmail.com>
2026-08-31 12:42:08 +0530

    Prepare backend for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,37 +1,34 @@
-require('dotenv').config();
-
-// ==================== Imports ====================
+require("dotenv").config();
 
 const http = require("http");
 
 const connectDB = require("./config/db");
-const app = require('./app')
-
-
+const app = require("./app");
 
 const {
   initializeSocket,
 } = require("./config/socket");
 
 
-
-// ==================== Create HTTP Server ====================
+// Create HTTP server
 
 const server = http.createServer(app);
 
-// ==================== Initialize Socket.IO ====================
+
+// Initialize Socket.IO
 
 initializeSocket(server);
 
 
-
-
-
 // Connect to MongoDB
-connectDB()
 
-const PORT = process.env.PORT
+connectDB();
 
-server.listen(PORT, () => {
-  console.log(`Running at PORT http://localhost:${PORT}`)
-})
+
+// Start server
+
+const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
